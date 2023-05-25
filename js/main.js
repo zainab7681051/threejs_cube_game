4b78ec2834f9f8e863bf109e22e07c518dacf122
zainab7681051 <zainab76810510@gmail.com>
2023-05-26 01:24:05 +0300

    m

--- js/main.js
+++ js/main.js
@@ -44,19 +44,27 @@ class Box extends THREE.Mesh {
     this.width = width;
     this.depth = depth;
     this.velocity = velocity;
-
     this.position.set(position.x, position.y, position.z);
+    this.gravity = -0.002;
+
     this.bottom = this.position.y - this.height / 2; //bottom value of the mesh object
     this.top = this.position.y + this.height / 2; //top value of the mesh object
     this.right = this.position.x + this.width / 2;
     this.left = this.position.x - this.width / 2;
-
-    this.gravity = -0.002;
+    this.front = this.position.z + this.depth / 2;
+    this.back = this.position.z - this.depth / 2;
   }
 
-  update(ground) {
+  updateSides() {
     this.bottom = this.position.y - this.height / 2; //bottom value of the mesh object
     this.top = this.position.y + this.height / 2; //top value of the mesh object
+    this.right = this.position.x + this.width / 2;
+    this.left = this.position.x - this.width / 2;
+    this.front = this.position.z + this.depth / 2;
+    this.back = this.position.z - this.depth / 2;
+  }
+  update(ground) {
+    this.updateSides();
     this.position.x += this.velocity.x;
     this.position.z += this.velocity.z;
     this.applyGravity(ground); //gravity on the y-axis
@@ -64,15 +72,31 @@ class Box extends THREE.Mesh {
 
   applyGravity(ground) {
     this.velocity.y += this.gravity; //object's speed increases when falling down due to gravity
-    //predicting collision one frame before it actually occures
-    if (this.bottom + this.velocity.y <= ground.top) {
-      this.velocity.y += 0.8;
-      this.velocity.y = -this.velocity.y; //reversing velocity-going down to going up so as to add bumping effect
+    if (
+      boxCollision({
+        box1: this,
+        box2: ground,
+      })
+    ) {
+      this.velocity.y += 0.8; //apllying friction; cube bounces off of the ground couple of times after colliding but eventually stops due to friction
+      this.velocity.y = -this.velocity.y; //reversing velocity from going down to going up so as to add the bouncing effect
     } else {
       this.position.y += this.velocity.y;
     }
   }
 }
+
+function boxCollision({ box1, box2 }) {
+  //detect for collision
+  const xCollision = box1.right >= box2.left && box1.left <= box2.right;
+  const yCollision =
+    box1.bottom + box1.velocity.y <= box2.top && box1.top >= box2.bottom; //box1.bottom + box1.velocity.y is for predicting collison on the y-axis one frame before it happens so there will not be any overflow between the bottom of the cube and top of the ground
+  const zCollision = box1.front >= box2.back && box1.back <= box2.front;
+  if (xCollision && yCollision && zCollision) {
+    return true;
+  }
+}
+
 const cube = new Box({
   width: 1,
   height: 1,
@@ -163,6 +187,28 @@ window.addEventListener("keyup", (event) => {
   }
 });
 
+const enemy = new Box({
+  width: 1,
+  height: 1,
+  depth: 1,
+  color: 0xff3131,
+  position: {
+    x: 0,
+    y: 0,
+    z: -4,
+  },
+  velocity: {
+    x: 0,
+    y: -0.1,
+    z: 0,
+  },
+});
+
+enemy.castShadow = true;
+scene.add(enemy);
+
+const enemies = [enemy];
+
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
@@ -170,11 +216,14 @@ function animate() {
   cube.velocity.x = 0;
   cube.velocity.z = 0;
 
-  if (keys.a.pressed) cube.velocity.x = -0.1;
-  else if (keys.d.pressed) cube.velocity.x = 0.1;
-  if (keys.s.pressed) cube.velocity.z = 0.1;
-  else if (keys.w.pressed) cube.velocity.z = -0.1;
+  if (keys.a.pressed) cube.velocity.x = -0.5;
+  else if (keys.d.pressed) cube.velocity.x = 0.5;
+  if (keys.s.pressed) cube.velocity.z = 0.5;
+  else if (keys.w.pressed) cube.velocity.z = -0.5;
 
   cube.update(ground);
+  enemies.forEach((enemy) => {
+    enemy.update(ground);
+  });
 }
 animate();
